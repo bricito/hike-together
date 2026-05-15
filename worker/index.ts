de022a491299fa7e82d07141e5f6ca32ebd2c4a3
bricito <bricenum@gmail.com>
2026-05-15 14:55:14 +0200

    Update index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -1,18 +1,16 @@
 export default {
-  async fetch(request, env) {
+  async fetch(request) {
     const url = new URL(request.url);
 
     if (url.pathname === "/generate") {
       const hikeId = url.searchParams.get("hikeId");
 
       const token = crypto.randomUUID();
-      const expiresAt = Date.now() + 60 * 1000; // 60 sec
 
-      // ici tu pourrais update Supabase via API
       return Response.json({
         hikeId,
         token,
-        expiresAt,
+        expiresAt: Date.now() + 30000,
       });
     }
 
