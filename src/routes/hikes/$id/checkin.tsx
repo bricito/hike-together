d5bcdae5648703e5a1c834729400fe659aeb3aea
bricito <bricenum@gmail.com>
2026-05-15 16:24:09 +0200

    Delete src/routes/hikes/$id directory

--- src/routes/hikes/$id/checkin.tsx
+++
@@ -1,8 +0,0 @@
-export default function CheckinPage() {
-  return (
-    <div>
-      <h1>📱 Check-in randonnée</h1>
-      <p>Scanner le QR code ou entrer le code manuellement</p>
-    </div>
-  );
-}
--- src/routes/hikes/$id/manage.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import ManageHike from "@/pages/ManageHike";
-
-export const Route = createFileRoute("/hikes/$id/manage")({
-  component: ManageHike,
-});
