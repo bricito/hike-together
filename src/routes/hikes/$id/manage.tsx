670459d1614f890b8398f9758ba9c88e1e3826da
bricito <bricenum@gmail.com>
2026-05-15 16:22:44 +0200

    Update import path for ManageHike component

--- src/routes/hikes/$id/manage.tsx
+++ src/routes/hikes/$id/manage.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import ManageHike from "../../../pages/ManageHike";
+import ManageHike from "@/pages/ManageHike";
 
 export const Route = createFileRoute("/hikes/$id/manage")({
   component: ManageHike,
