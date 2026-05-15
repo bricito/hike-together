4b6f6dddac8c886bacb4a29623dcd9a6b8cd7e71
bricito <bricenum@gmail.com>
2026-05-15 15:53:58 +0200

    Add CheckinPage component for hike check-in

---
+++ src/routes/hikes/$id/src/routes/checkin.tsx
@@ -0,0 +1,8 @@
+export default function CheckinPage() {
+  return (
+    <div>
+      <h1>📱 Check-in randonnée</h1>
+      <p>Scanner le QR code ou entrer le code manuellement</p>
+    </div>
+  );
+}
