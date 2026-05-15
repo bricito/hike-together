76397cb01eb4b194cb736a924021b37ba6d2d21a
bricito <bricenum@gmail.com>
2026-05-15 16:12:10 +0200

    Update manage.tsx

--- src/routes/hikes/$id/manage.tsx
+++ src/routes/hikes/$id/manage.tsx
@@ -1,14 +1,6 @@
-import { useParams } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
+import ManageHike from "../../../pages/ManageHike";
 
-export default function ManageHike() {
-  const { id } = useParams({ from: "/hikes/$id/manage" });
-
-  return (
-    <div>
-      <h1>🧑‍💼 Gestion randonnée</h1>
-      <p>Hike ID: {id}</p>
-
-      {/* ici plus tard : QR code + check-in + participants */}
-    </div>
-  );
-}
+export const Route = createFileRoute("/hikes/$id/manage")({
+  component: ManageHike,
+});
