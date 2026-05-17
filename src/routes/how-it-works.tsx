4693152f06b070d00d4ec25a3e23d88de5ba93b1
bricito <bricenum@gmail.com>
2026-05-17 12:12:55 +0200

    Update how-it-works.tsx

--- src/routes/how-it-works.tsx
+++ src/routes/how-it-works.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
-import { SiteHeader } from "@/components/site-header";
-import { SiteFooter } from "@/components/site-footer";
+import { SiteHeader } from "@/components/SiteHeader";
+import { SiteFooter } from "@/components/SiteFooter";
 
 import {
   Mountain,
