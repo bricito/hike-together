655d04b0b4b3bcb6cf9539ee5d76c23dbb30f808
bricito <bricenum@gmail.com>
2026-05-17 10:50:31 +0200

    Update manage.tsx

--- src/pages/hikes/$id/manage.tsx
+++ src/pages/hikes/$id/manage.tsx
@@ -1,3 +1,6 @@
-import ManageHike from "../../organizer/ManageHike";
+import { createFileRoute } from "@tanstack/react-router";
+import ManageHike from "../../../pages/organizer/ManageHike";
 
-export default ManageHike;
+export const Route = createFileRoute("/hikes/$id/manage")({
+  component: ManageHike,
+});
