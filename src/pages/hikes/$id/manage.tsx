2a044e46a2749c56d5135c38872f70a0e318e687
bricito <bricenum@gmail.com>
2026-05-17 10:59:09 +0200

    Remove ManageHike route definition

--- src/pages/hikes/$id/manage.tsx
+++ src/pages/hikes/$id/manage.tsx
@@ -1,6 +1,1 @@
-import { createFileRoute } from "@tanstack/react-router";
-import ManageHike from "../../../pages/organizer/ManageHike";
 
-export const Route = createFileRoute("/hikes/$id/manage")({
-  component: ManageHike,
-});
