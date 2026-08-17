57fd4fea8e671116cbecf1d79f0489c5e9c65bf6
bricito <bricenum@gmail.com>
2026-08-17 11:16:41 +0200

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"; 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   Outlet,
